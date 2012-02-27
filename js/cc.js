3bc3da90736a9565ad8718a0dd97aca61433025e
Bala Clark <balaclark@gmail.com>
2012-02-27 16:37:10 +0100

    add cards to a deck

---
+++ js/cc.js
@@ -0,0 +1,61 @@
+
+var Stacked = { Type: {}, Deck: null };
+
+(function () {
+
+	'use strict';
+
+	Stacked.Type.Base = Backbone.Model.extend({
+		defaults: {
+			cards: [],
+			suites: []
+		}
+	});
+
+	Stacked.Type.Spanish = Stacked.Type.Base.extend({
+		defaults: {
+			cards: [1, 2, 3, 4, 5, 6, 7, 10, 11, 12],
+			suites: ['cups', 'clubs', 'coins', 'swords']
+		}
+	});
+
+	Stacked.Card = Backbone.Model.extend({
+		defualts: {
+			type: '',
+			suite: '',
+			number: 0
+		}
+	});
+
+	Stacked.Cards = Backbone.Collection.extend({
+		model: Stacked.Card
+	});
+
+	Stacked.Deck = Backbone.Model.extend({
+
+		defaults: {
+			decks: 1,
+			type: null
+		},
+
+		initialize: function () {
+
+			var type = this.get('type'),
+				set  = new Stacked.Type[type](),
+				cards = new Stacked.Cards();
+
+			this.set({ set: set });
+
+			for (var i = 0; i < this.get('decks'); i++) {
+				_.each(set.get('suites'), function (suite) {
+					_.each(set.get('cards'), function (card) {
+						cards.add({ type: type, suite: suite, card: card});
+					});
+				});
+			}
+
+			this.set({ cards: cards });
+		}
+	});
+
+})();
