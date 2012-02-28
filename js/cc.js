4a9ece96fe35da57bb2cecec987243668950c1e4
Bala Clark <balaclark@gmail.com>
2012-02-28 21:25:43 +0100

    save & re-render changed order when dragging cards

--- js/cc.js
+++ js/cc.js
@@ -93,7 +93,14 @@ Stacked.Collection = {};
 	});
 
 	Stacked.Collection.Hand = Backbone.Collection.extend({
-		model: Stacked.Model.Card
+
+		model: Stacked.Model.Card,
+
+		updateValues: function (values) {
+			$.each(values, _.bind(function (key, attributes) {
+				this.at(key).set(attributes);
+			}, this));
+		}
 	});
 
 	Stacked.Model.Player = Backbone.Model.extend({
@@ -160,11 +167,15 @@ Stacked.Collection = {};
 
 	Stacked.View.Card = Backbone.View.extend({
 
+		events: {
+			'change': 'render'
+		},
+
 	 	render: function () {
 
 	 		var data = this.model.toJSON();
 
-	 		data.id = data.id || this.model.cid;
+	 		data.cid = this.model.cid;
 
 	 		this.$el.append(ich.card(data));
 	 	}
@@ -174,14 +185,35 @@ Stacked.Collection = {};
 	Stacked.View.Hand = Backbone.View.extend({
 
 		tagName: 'ul',
+
 		className: 'hand',
 
-	 	render: function () {
+		events: {
+			'sortstop ': 'reorder'
+		},
+
+		cards: [],
+
+		initialize: function () {
+
+			var el = this.el,
+				cards = [];
+
+			this.model.each(function (card) {
+				cards.push(new Stacked.View.Card({ el: el, model: card }));
+			});
+
+			this.cards = cards;
+		},
+
+	 	render: function (ddd) {
 
 	 		var el = this.el;
 
-			this.model.each(function (card) {
-				(new Stacked.View.Card({ el: el, model: card })).render();
+	 		this.$el.empty();
+
+			_.each(this.cards, function (card) {
+				card.render();
 			});
 
 			this.$el.sortable({
@@ -191,6 +223,19 @@ Stacked.Collection = {};
 				tolerance: 'pointer',
 				containment: 'parent'
 			});
+	 	},
+
+	 	reorder: function (event, ui) {
+
+	 		var values = [];
+
+	 		this.$('.card').each(function (i, elm) {
+	 			values.push($(elm).data());
+	 		});
+
+	 		this.model.updateValues(values);
+
+	 		this.render();
 	 	}
 	});
 
